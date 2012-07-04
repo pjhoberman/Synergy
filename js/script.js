e6d677e6bf0091da51a85d1c8af54108de7bda26
PJ Hoberman <pj.hoberman@gmail.com>
2012-07-04 10:40:42 -0600

    slider

--- js/script.js
+++ js/script.js
@@ -1,11 +1,63 @@
-/* Author:
+/* Site variables - change these to whatever you want */
+var slide_delay = 5; // this is in seconds;
 
-*/
+/* Edit below this line with caution */
+
+var current_slide = 1,
+    slide_interval;
 
 function setActiveNav(){
-    console.log(window.location);
+    var path = window.location.pathname.split('/'),
+        page = path[path.length-1].replace('.html','');
+
+    $('nav li.active').removeClass('active');
+    $('nav li a[href*=' + page + ']').parent().addClass('active');
+}
+
+function changeSlide(n){
+    if(n > $('#image-slider .image-slider-button').length || n === 0 ){
+        n = 1;
+    }
+
+    current_slide = n;
+    $('#image-slider img').attr('src', 'img/slider/' + n + '.png');
+    $('#image-slider .image-slider-button').removeClass('active');
+    $('#image-slider .image-slider-button:eq(' + (n-1) + ')').addClass('active');
+}
+
+function setSlideInterval(){
+    clearInterval(slide_interval);
+    slide_interval = setInterval( function(){changeSlide(current_slide+1);}, slide_delay * 1000 );
 }
 
  $(function() {
+    // make the logo clickable
+    $('#logo').click(function(){
+        window.location = '/';
+    });
+
+    // set up the active state of the nav
     setActiveNav();
+
+    // set up nav links
+    $('nav li').click(function(){
+        window.location = $(this).find('a').attr('href');
+    });
+
+    // image slider
+
+        // click on a button
+        $('#image-slider .image-slider-button').click(function(){
+            changeSlide($(this).index() + 1);
+        });
+
+        // start the interval
+        setSlideInterval();
+
+        $('#image-slider').mouseover(function(){
+            clearInterval(slide_interval);
+        })
+        .mouseout(function(){
+            setSlideInterval();
+        });
  });
